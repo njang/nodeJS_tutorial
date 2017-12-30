ae362868d12ca4d41fac0f7e7c6b33d2081ee284
njang <neo.w.jang@gmail.com>
2017-12-29 22:22:02 -0600

    Revise previous practice

--- demo_module.js
+++ demo_module.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./myFirstModule');
 
 http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('Hello World!');
+	res.write('The current date and time are ' + dt.myDateTime());
+	res.end();
 }).listen(8080);
