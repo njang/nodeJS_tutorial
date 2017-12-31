de944e8073c2e1842b71a4a0381e9571788e1583
njang <neo.w.jang@gmail.com>
2017-12-30 22:38:43 -0600

    HTTP module

--- demo_module.js
+++ demo_module.js
@@ -1,8 +1,13 @@
 var http = require('http');
 var dt = require('./myFirstModule');
+var url = require('url');
 
 http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('The current date and time are ' + dt.myDateTime());
-	res.end();
+	// res.write('The current date and time are ' + dt.myDateTime());
+	// res.write(req.url);
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+	// res.end();
+	res.end(txt);
 }).listen(8080);
